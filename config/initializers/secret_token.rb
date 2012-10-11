b716ae3133bc3828243b69f9f9f13065fb9c7430
David Padilla <david@crowdint.com>
2012-10-10 21:01:06 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FundraiserApp::Application.config.secret_token = '344b0efca573438a5255564ae460e0619dc474435f593b0119a407fde1888f730d35cdcfa5bd4d947d68543dd4073213e2b6d82cf2b53d74d7102400ffd0b360'
