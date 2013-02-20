21e5b8a7d17fd4812d8817ad79e9d076ee7bd042
Hector Bustillos <hector.bustillos@crowdint.com>
2013-02-19 23:38:00 -0600

    change for load crowdblog assets

--- app/assets/javascripts/crowdblog.js
+++ app/assets/javascripts/crowdblog.js
@@ -1,11 +1,10 @@
 /*
- *= require         'jquery_ujs'
  *= require         'backbone_rails_sync'
  *= require         'markdown'
  *= require         'uploader/jquery.html5uploader.js'
  *= require_self
- *= require_tree    "./crowdblog"
  *= require         "crowdblog/models/post"
+ *= require_tree    ./crowdblog
  */
 
 window.Crowdblog        = {};
