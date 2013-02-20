af9e20ae7f7067a45df6aa67901318ace7e6ccb4
Hector Bustillos <hector.bustillos@crowdint.com>
2013-02-20 09:52:28 -0600

    Remove 'overriden' assets and make a workaround in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ FundraiserApp::Application.routes.draw do
 
   mount Fundraiser::Engine => "/"
   mount Crowdblog::Engine => "/blog"
+
+  match '/admin/posts/:id/:transition', :to => 'crowdblog/admin/transitions#create',
+    :constraints => { :transition => /(draft|finish|review|publish)/ },
+    :as => 'post_transitions',
+    :via => :post
 end
