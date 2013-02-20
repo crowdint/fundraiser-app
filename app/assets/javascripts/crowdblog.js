2816706345a2c576ada4734e9c524e83d02f1b9f
Hector Bustillos <hector.bustillos@crowdint.com>
2013-02-19 23:17:04 -0600

    PATCH to load custom overriden js for crowdblog

---
+++ app/assets/javascripts/crowdblog.js
@@ -0,0 +1,13 @@
+/*
+ *= require         'jquery_ujs'
+ *= require         'backbone_rails_sync'
+ *= require         'markdown'
+ *= require         'uploader/jquery.html5uploader.js'
+ *= require_self
+ *= require_tree    "./crowdblog"
+ *= require         "crowdblog/models/post"
+ */
+
+window.Crowdblog        = {};
+window.Crowdblog.Models = {};
+window.Crowdblog.Views  = {};
