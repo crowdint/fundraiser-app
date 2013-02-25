1a20ae03e6772238728ba05088775d80d0abdb5b
David Padilla <david@crowdint.com>
2013-02-25 12:49:44 -0800

    The good Asset Hosts commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ FundraiserApp::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://k%d.magmaconf.com"
+  config.action_controller.asset_host = Proc.new { |source| $ASSET_HOSTS[rand(4)] }
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
---
+++ config/initializers/asset_hosts.rb
@@ -0,0 +1,4 @@
+$ASSET_HOSTS = ['https://d2z9dunm5yy6xl.cloudfront.net',
+                'https://d3v0d76tbdapy9.cloudfront.net',
+                'https://d2kmxthgcjquv6.cloudfront.net',
+                'https://d2erir56vvfg0t.cloudfront.net']
