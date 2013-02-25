95eafd41d0fdf80ee21b5939be881125856ba493
David Padilla <david@crowdint.com>
2013-02-25 12:05:40 -0800

    Different asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ FundraiserApp::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://assets%d.magmaconf.com"
+  config.action_controller.asset_host = "http://k%d.magmaconf.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
